7ffcc51c00722ea1df7e3887854111eb9de6f057
kemper-dev <collinos@proton.me>
2023-01-17 14:45:27 -0500

    react router setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex flex-col justify-center px-8">
+      <nav className="flex items-center justify-between w-full relative max-w-2xl mx-auto pt-8 pb-8 sm:pb-16">
+        <div>
+          <NavLink 
+            to="/" 
+            className={({ isActive }) =>
+              isActive 
+              ? "font-semibold"
+              : "font-normal"
+            }
+          >
+            Home
+          </NavLink>
+        </div>
+        <div>
+          <NavLink 
+            to="/profile" 
+            className={({ isActive }) =>
+              isActive 
+              ? "font-semibold"
+              : "font-normal"
+            }
+          >
+            Profile
+          </NavLink>
+          <NavLink 
+            to="/test" 
+            className={({ isActive }) =>
+              isActive 
+              ? "font-semibold"
+              : "font-normal"
+            }
+          >
+            Search
+          </NavLink>
+        </div>
+      </nav>
+    </div>
   )
 }
 
