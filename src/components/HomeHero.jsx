de125cdb6adc4625cf8903be352ebcfaa0dd6aea
kemper-dev <collinos@proton.me>
2023-02-02 16:30:08 -0500

    update

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -6,6 +6,8 @@ const HomeHero = () => {
       <h1 className="text-4xl font-bold">
         A dystopian future where human life is viewed as a game.
         Value is determined by your skill card. 
+        {/* The year is 2049. AI have taken control of the world and conditioned humans to view
+        their lives as a game. You will now go through the screening process to be assigned your skill card. */}
       </h1>
       <div className="p-4"/>
       <button className="border border-red-500 hover:bg-red-500 rounded-full p-2 w-24 duration-200">Create</button>
