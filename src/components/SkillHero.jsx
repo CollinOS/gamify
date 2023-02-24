2a4e76d548b0ee9d720f67578dfd739bf3885b1d
kemper-dev <collinos@proton.me>
2023-02-24 16:43:19 -0500

    Update SkillHero.jsx

---
+++ src/components/SkillHero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SkillHero = () => {
+  return (
+    <div>
+      <p>
+        Answer the questions below and use the sliders to estimate where you are with each skill. Once you are done,
+        submit the form to downlaod your skill card.
+      </p>
+    </div>
+  )
+}
+
+export default SkillHero
